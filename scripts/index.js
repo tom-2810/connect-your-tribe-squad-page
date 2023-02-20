f1991e7699ba51a26fab60a4a3e6ee6b27d0a735
tom-2810 <tom.poortman@hva.nl>
2023-02-20 14:25:31 +0100

    banner update

--- scripts/index.js
+++ scripts/index.js
@@ -18,9 +18,9 @@ app.use(express.static('public'))
 
 // Maak een route voor de index
 app.get('/', (request, response) => {
-  console.log(request.query.squad)
+  // console.log(request.query.squad)
 
-  let slug = request.query.squad || 'squad-a-2022'
+  let slug = request.query.squad || 'squad-b-2022'
   let orderBy = request.query.orderBy || 'name'
   let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
 
