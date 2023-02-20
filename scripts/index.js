7fe7cc1fcded02ba53d561527f0bd53136116f07
tom-2810 <tom.poortman@hva.nl>
2023-02-20 12:30:11 +0100

    close #4 . squad navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 
-const url = 'https://whois.fdnd.nl/api/v1/squad/squad-b-2022'
-const data = await fetch(url)
-  .then((response) => response.json())
-  .catch((error) => error)
+const url = 'https://whois.fdnd.nl/api/v1/squad/'
+// const data = await fetch(url)
+//   .then((response) => response.json())
+//   .catch((error) => error)
 
-console.log(data)
+// console.log(data)
+
 
 // Maak een nieuwe express app
 const app = express()
@@ -19,7 +20,14 @@ app.use(express.static('public'))
 app.get('/', (request, response) => {
   console.log(request.query.squad)
 
-  response.render('index', data)
+  let slug = request.query.squad || 'squad-a-2022'
+  let orderBy = request.query.orderBy || 'name'
+  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
+
+
+  fetchJson(squadUrl).then((data) => {
+    response.render('index', data)
+  })
 })
 
 // app.get('/partials/members', (request, response) => {
@@ -31,3 +39,14 @@ app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
